a634a541cb5d680924c4feb4d3b825d07d9b6e81
Alexey Shepelev <al.shepelev@gmail.com>
2012-03-21 13:12:05 +0300

    model: distribution save

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -12,16 +12,20 @@ require 'waybill'
 class Distribution < ActiveRecord::Base
   has_paper_trail
 
+  UNKNOWN = 0
+  INWORK = 1
+
   validates :foreman, :foreman_place, :storekeeper, :storekeeper_place,
-            :created, :state, :presence => true
+            :created, :state, presence: true
 
   belongs_to :deal
-  belongs_to :foreman, :polymorphic => true
-  belongs_to :storekeeper, :polymorphic => true
-  belongs_to :foreman_place, :class_name => 'Place'
-  belongs_to :storekeeper_place, :class_name => 'Place'
+  belongs_to :foreman, polymorphic: true
+  belongs_to :storekeeper, polymorphic: true
+  belongs_to :foreman_place, class_name: 'Place'
+  belongs_to :storekeeper_place, class_name: 'Place'
 
   after_initialize :do_after_initialize
+  before_save :do_before_save
 
   def add_item(tag, mu, amount)
     resource = Asset.find_by_tag_and_mu(tag, mu)
@@ -29,12 +33,49 @@ class Distribution < ActiveRecord::Base
   end
 
   def items
+    if @items.empty? and !self.deal.nil?
+      self.deal.rules.each { |rule|
+        @items << DistributionItem.new(rule.from.take.resource, rule.rate)
+      }
+    end
     @items
   end
 
   private
   def do_after_initialize
     @items = Array.new
+    self.state = UNKNOWN
+  end
+
+  def do_before_save
+    if self.new_record?
+      shipment = Asset.find_or_create_by_tag('Warehouse Shipment')
+      self.deal = Deal.new(entity: self.storekeeper, rate: 1.0, isOffBalance: true,
+        tag: "Distribution shipment ##{Distribution.last.nil? ? 0 : Distribution.last.id}")
+      return false if self.deal.build_give(place: self.storekeeper_place,
+                                           resource: shipment).nil?
+      return false if self.deal.build_take(place: self.foreman_place,
+                                           resource: shipment).nil?
+      return false unless self.deal.save
+      self.deal_id = self.deal.id
+
+      @items.each { |item, idx|
+        storekeeper_item = item.warehouse_deal(nil, self.storekeeper_place,
+                                               self.storekeeper)
+        return false if storekeeper_item.nil?
+
+        foreman_item = item.warehouse_deal(nil, self.foreman_place, self.foreman)
+        return false if foreman_item.nil?
+
+        return false if self.deal.rules.create(tag: "#{deal.tag}; rule#{idx}",
+          from: storekeeper_item, to: foreman_item, fact_side: false,
+          change_side: true, rate: item.amount).nil?
+      }
+      self.state = INWORK if self.state == UNKNOWN
+    else
+      return false
+    end
+    true
   end
 end
 
--- spec/models/distribution_spec.rb
+++ spec/models/distribution_spec.rb
@@ -10,6 +10,15 @@
 require 'spec_helper'
 
 describe Distribution do
+  before(:all) do
+    Factory(:chart)
+    @wb = Factory.build(:waybill)
+    @wb.add_item('roof', 'm2', 500, 10.0)
+    @wb.add_item('nails', 'pcs', 1200, 1.0)
+    @wb.add_item('nails', 'kg', 10, 150.0)
+    @wb.save!
+  end
+
   it 'should have next behaviour' do
     should validate_presence_of :foreman
     should validate_presence_of :storekeeper
@@ -25,16 +34,114 @@ describe Distribution do
   end
 
   it 'should create items' do
-    Factory(:chart)
-
-    wb = Factory.build(:waybill)
-    wb.add_item('nails', 'pcs', 1200, 1.0)
-    wb.add_item('nails', 'kg', 10, 150.0)
-    wb.save!
-
     db = Factory.build(:distribution)
-    db.add_item("nails", "pcs", 500)
-    db.add_item("nails", "kg", 2)
+    db.add_item('nails', 'pcs', 500)
+    db.add_item('nails', 'kg', 2)
     db.items.count.should eq(2)
+    lambda { db.save } .should change(Distribution, :count).by(1)
+    db.items[0].resource.should eq(Asset.find_all_by_tag_and_mu('nails', 'pcs').first)
+    db.items[0].amount.should eq(500)
+    db.items[1].resource.should eq(Asset.find_all_by_tag_and_mu('nails', 'kg').first)
+    db.items[1].amount.should eq(2)
+
+    db = Distribution.find(db)
+    db.items.count.should eq(2)
+    db.items[0].resource.should eq(Asset.find_all_by_tag_and_mu('nails', 'pcs').first)
+    db.items[0].amount.should eq(500)
+    db.items[1].resource.should eq(Asset.find_all_by_tag_and_mu('nails', 'kg').first)
+    db.items[1].amount.should eq(2)
+  end
+
+  it 'should create deals' do
+    db = Factory.build(:distribution)
+    db.add_item('roof', 'm2', 200)
+    lambda { db.save } .should change(Deal, :count).by(3)
+
+    deal = Deal.find(db.deal)
+    deal.entity.should eq(db.storekeeper)
+    deal.isOffBalance.should be_true
+
+    deal = db.items.first.warehouse_deal(nil, db.storekeeper_place, db.storekeeper)
+    deal.should_not be_nil
+    deal.rate.should eq(1.0)
+    deal.isOffBalance.should be_true
+
+    deal = db.items.first.warehouse_deal(nil, db.foreman_place, db.foreman)
+    deal.should_not be_nil
+    deal.rate.should eq(1.0)
+    deal.isOffBalance.should be_true
+
+    wb = Factory.build(:waybill, distributor: @wb.distributor,
+                                 distributor_place: @wb.distributor_place,
+                                 storekeeper: @wb.storekeeper,
+                                 storekeeper_place: @wb.storekeeper_place)
+    wb.add_item('roof', 'm2', 100, 10.0)
+    wb.add_item('hammer', 'th', 500, 100.0)
+    lambda { wb.save } .should change(Deal, :count).by(3)
+
+    db = Factory.build(:distribution, storekeeper: db.storekeeper,
+                                      storekeeper_place: db.storekeeper_place)
+    db.add_item('roof', 'm2', 200)
+    db.add_item('nails', 'pcs', 1200)
+    db.add_item('hammer', 'th', 500)
+    lambda { db.save } .should change(Deal, :count).by(6)
+
+    deal = Deal.find(db.deal)
+    deal.entity.should eq(db.storekeeper)
+    deal.isOffBalance.should be_true
+
+    db.items.each { |i|
+      deal = i.warehouse_deal(nil, db.storekeeper_place, db.storekeeper)
+      deal.should_not be_nil
+      deal.rate.should eq(1.0)
+      deal.isOffBalance.should be_true
+
+      deal = i.warehouse_deal(nil, db.foreman_place, db.foreman)
+      deal.should_not be_nil
+      deal.rate.should eq(1.0)
+      deal.isOffBalance.should be_true
+    }
+  end
+
+  it 'should create rules' do
+    db = Factory.build(:distribution)
+    db.add_item('roof', 'm2', 200)
+    lambda { db.save } .should change(Rule, :count).by(1)
+
+    rule = db.deal.rules.first
+    rule.rate.should eq(200)
+    db.items.first.warehouse_deal(nil, db.storekeeper_place,
+      db.storekeeper).should eq(rule.from)
+    db.items.first.warehouse_deal(nil, db.foreman_place,
+      db.foreman).should eq(rule.to)
+
+    db = Factory.build(:distribution, storekeeper: db.storekeeper,
+                                      storekeeper_place: db.storekeeper_place)
+    db.add_item('roof', 'm2', 150)
+    db.add_item('nails', 'pcs', 300)
+    lambda { db.save } .should change(Rule, :count).by(2)
+
+    rule = db.deal.rules[0]
+    rule.rate.should eq(150)
+    db.items[0].warehouse_deal(nil, db.storekeeper_place,
+      db.storekeeper).should eq(rule.from)
+    db.items[0].warehouse_deal(nil, db.foreman_place,
+      db.foreman).should eq(rule.to)
+
+    rule = db.deal.rules[1]
+    rule.rate.should eq(300)
+    db.items[1].warehouse_deal(nil, db.storekeeper_place,
+      db.storekeeper).should eq(rule.from)
+    db.items[1].warehouse_deal(nil, db.foreman_place,
+      db.foreman).should eq(rule.to)
+  end
+
+  it 'should change state' do
+    db = Factory.build(:distribution)
+    db.add_item('roof', 'm2', 200)
+    db.state.should eq(Distribution::UNKNOWN)
+
+    db.save!
+    db.state.should eq(Distribution::INWORK)
   end
 end
